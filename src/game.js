ec39d3aea85f6cc99b9460ffdb2bcb53c17637a8
syashakash <akashshivram9@gmail.com>
2019-01-29 00:35:23 +0530

    Add modularity

---
+++ src/game.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Board from './board';
+import './index.css';
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>
+                        {/*status*/}
+                    </div>
+                    <div>
+                        {/*TO DO*/}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Game;
